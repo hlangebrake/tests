673be7882e30a7338e4c77d8a2c430aba9378dd8
hlangebrake <hendrik.langebrake@gidw-online.de>
2026-09-10 10:52:49 +0200

    Delete Kommaland-Web-App directory

--- Kommaland-Web-App/teacher-dashboard/js/analysis-worker.js
+++
@@ -1,3 +0,0 @@
-import {TeacherDB} from './database.js';
-import {report} from './report.js';
-self.onmessage=async({data})=>{const db=new TeacherDB(data.databaseName);try{const result=await report(db,data.options);postMessage({id:data.id,result});}catch(e){postMessage({id:data.id,error:e.message});}finally{try{(await db.ready).close();}catch{}}};
